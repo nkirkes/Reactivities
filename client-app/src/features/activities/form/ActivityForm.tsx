f36a80379772b13087e1607793d8cecbecf25581
Nick Kirkes <nick.kirkes@gmail.com>
2020-04-15 13:29:16 -0400

    section 10.140, adds TextAreaInput component.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,6 +7,7 @@ import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router-dom";
 import { Form as FinalForm, Field } from "react-final-form";
 import TextInput from "../../../app/common/form/TextInput";
+import TextAreaInput from "../../../app/common/form/TextAreaInput";
 
 interface DetailParams {
   id: string;
@@ -89,10 +90,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                 />
                 <Field
                   name="description"
-                  rows={2}
+                  rows={3}
                   placeholder="Description"
                   value={activity.description}
-                  component={TextInput}
+                  component={TextAreaInput}
                 />
                 <Field
                   name="category"
