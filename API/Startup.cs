278f1d7528a84057a62f7285153dbecc0db5183d
Nick Kirkes <nick.kirkes@gmail.com>
2020-04-17 03:09:16 -0400

    section 12.163, setting staging for token generation with new Infrastructure csproj, and application interface.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -48,7 +50,8 @@ namespace API
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
 
-            services.AddAuthentication();
+            // services.AddAuthentication();
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
