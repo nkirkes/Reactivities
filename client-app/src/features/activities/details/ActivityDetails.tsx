50b8ff7fe452bd60fd4d12937a9421996397e6f6
Nick Kirkes <nick.kirkes@gmail.com>
2020-04-17 14:05:03 -0400

    section 13.178, adds client side user objects, moves stores to rootStore, rewires components using activityStore.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,51 +1,47 @@
-import React, { useContext, useEffect } from "react";
-import { Grid } from "semantic-ui-react";
-import ActivityStore from "../../../app/stores/activityStore";
-import { observer } from "mobx-react-lite";
-import { RouteComponentProps } from "react-router-dom";
-import { LoadingComponent } from "../../../app/layout/LoadingComponent";
-import ActivityDetailHeader from "./ActivityDetailHeader";
-import ActivityDetailInfo from "./ActivityDetailInfo";
-import ActivityDetailChat from "./ActivityDetailChat";
-import ActivityDetailSidebar from "./ActivityDetailSidebar";
+import React, { useContext, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { RouteComponentProps } from 'react-router-dom';
+import { LoadingComponent } from '../../../app/layout/LoadingComponent';
+import ActivityDetailHeader from './ActivityDetailHeader';
+import ActivityDetailInfo from './ActivityDetailInfo';
+import ActivityDetailChat from './ActivityDetailChat';
+import ActivityDetailSidebar from './ActivityDetailSidebar';
+import { RootStoreContext } from '../../../app/stores/rootStore';
 
 interface DetailParams {
-  id: string;
+    id: string;
 }
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
-  match,
-  history
+    match,
+    history,
 }) => {
-  const activityStore = useContext(ActivityStore);
-  const {
-    activity,
-    loadActivity,
-    loadingInitial,
-  } = activityStore;
+    const rootStore = useContext(RootStoreContext);
 
-  useEffect(() => {
-    loadActivity(match.params.id);
-  }, [loadActivity, match.params.id, history]);
+    const { activity, loadActivity, loadingInitial } = rootStore.activityStore;
 
-  if (loadingInitial)
-    return <LoadingComponent content="Loading activity..." />;
+    useEffect(() => {
+        loadActivity(match.params.id);
+    }, [loadActivity, match.params.id, history]);
 
-  if (!activity)
-    return <h2>Activity not found.</h2>
+    if (loadingInitial)
+        return <LoadingComponent content='Loading activity...' />;
 
-  return (
-    <Grid>
-      <Grid.Column width={10}>
-        <ActivityDetailHeader activity={activity} />
-        <ActivityDetailInfo activity={activity} />
-        <ActivityDetailChat />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <ActivityDetailSidebar />
-      </Grid.Column>
-    </Grid>
-  );
+    if (!activity) return <h2>Activity not found.</h2>;
+
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailHeader activity={activity} />
+                <ActivityDetailInfo activity={activity} />
+                <ActivityDetailChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailSidebar />
+            </Grid.Column>
+        </Grid>
+    );
 };
 
 export default observer(ActivityDetails);
