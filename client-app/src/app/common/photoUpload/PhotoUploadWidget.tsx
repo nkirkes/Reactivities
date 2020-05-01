70d0560ec0fdc7eee4508fefea547f7472b31a0a
nkirkes <nick.kirkes@gmail.com>
2020-05-01 14:59:28 -0400

    section 17.237, adds cropper functionality and style (part 1)

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -1,30 +1,44 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Header, Grid, Image } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
+import PhotoWidgetCropper from './PhotoWidgetCropper';
 
 const PhotoUploadWidget = () => {
-    const [files, setFiles] = useState<any[]>([]);
+  const [files, setFiles] = useState<any[]>([]);
+  const [image, setImage] = useState<Blob | null>(null);
 
-    return (
-      <Fragment>
-        <Grid>
-          <Grid.Column width={4}>
-            <Header color="teal" sub content="Step 1 - Add Photo" />
-            <PhotoWidgetDropzone setFiles={setFiles} />
-          </Grid.Column>
-          <Grid.Column width={1} />
-          <Grid.Column width={4}>
-            <Header sub color="teal" content="Step 2 - Resize image" />
-          </Grid.Column>
-          <Grid.Column width={1} />
-          <Grid.Column width={4}>
-            <Header sub color="teal" content="Step 3 - Preview & Upload" />
-            {files.length > 0 && <Image src={files[0].preview} />}
-          </Grid.Column>
-        </Grid>
-      </Fragment>
-    );
+  useEffect(() => {
+    return () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    };
+  });
+
+  return (
+    <Fragment>
+      <Grid>
+        <Grid.Column width={4}>
+          <Header color="teal" sub content="Step 1 - Add Photo" />
+          <PhotoWidgetDropzone setFiles={setFiles} />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 2 - Resize image" />
+          {files.length > 0 && (
+            <PhotoWidgetCropper
+              setImage={setImage}
+              imagePreview={files[0].preview}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 3 - Preview & Upload" />
+          {files.length > 0 && <Image src={files[0].preview} />}
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  );
 };
 
 export default observer(PhotoUploadWidget);
--- client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -1,26 +1,54 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { Header, Icon } from 'semantic-ui-react';
 
 interface IProps {
-    setFiles: (files: object[]) => void;
+  setFiles: (files: object[]) => void;
 }
 
-const PhotoWidgetDropzone: React.FC<IProps> = ({setFiles}) => {
+const dropzoneStyles = {
+  border: 'dashed 3px',
+  borderColor: '#eee',
+  borderRadius: '5px',
+  paddingTop: '30px',
+  textAlign: 'center' as 'center',
+  height: '200px',
+};
+
+const dropzoneActiveStyles = {
+  borderColor: 'green',
+};
+
+const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
   const onDrop = useCallback((acceptedFiles) => {
-    setFiles(acceptedFiles.map((file: object) => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-    })))
+    setFiles(
+      acceptedFiles.map((file: object) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div
+      {...getRootProps()}
+      style={
+        isDragActive
+          ? { ...dropzoneStyles, ...dropzoneActiveStyles }
+          : dropzoneStyles
+      }
+    >
       <input {...getInputProps()} />
-      {isDragActive ? (
+      <Icon name='upload' size='huge' />
+      <Header content='Drop image here...' />
+      
+      {/* {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+      )} */}
     </div>
   );
 };
