cbb1fe05047e591ab8337e22d53d3c9d1fb0127d
nkirkes <nick.kirkes@gmail.com>
2020-05-06 11:54:17 -0400

    section 18, challenge, UI updates for editing profile.

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -58,7 +58,7 @@ export default class ProfileStore {
       toast.error('Problem uploading photo.');
       runInAction(() => {
         this.uploadingPhoto = false;
-      })
+      });
     }
   };
 
@@ -68,32 +68,50 @@ export default class ProfileStore {
       await agent.Profiles.setMainPhoto(photo.id);
       runInAction(() => {
         this.rootStore.userStore.user!.image = photo.url;
-        this.profile!.photos.find(a => a.isMain)!.isMain = false;
-        this.profile!.photos.find(a => a.id === photo.id)!.isMain = true;
+        this.profile!.photos.find((a) => a.isMain)!.isMain = false;
+        this.profile!.photos.find((a) => a.id === photo.id)!.isMain = true;
         this.profile!.image = photo.url;
         this.loading = false;
-      })
+      });
     } catch (error) {
-      toast.error('Problem setting photo as main.')
+      toast.error('Problem setting photo as main.');
       runInAction(() => {
         this.loading = false;
-      })
+      });
     }
-  }
+  };
 
   @action deletePhoto = async (photo: IPhoto) => {
     this.loading = true;
     try {
       await agent.Profiles.deletePhoto(photo.id);
       runInAction(() => {
-        this.profile!.photos = this.profile!.photos.filter(a => a.id !== photo.id);
+        this.profile!.photos = this.profile!.photos.filter(
+          (a) => a.id !== photo.id
+        );
         this.loading = false;
-      })
+      });
     } catch (error) {
       toast.error('Problem deleting photo.');
       runInAction(() => {
         this.loading = false;
-      })
+      });
     }
-  }
+  };
+
+  @action updateProfile = async (profile: Partial<IProfile>) => {
+    try {
+      await agent.Profiles.updateProfile(profile);
+      runInAction(() => {
+        if (
+          profile.displayName !== this.rootStore.userStore.user!.displayName
+        ) {
+          this.rootStore.userStore.user!.displayName = profile.displayName!;
+        }
+        this.profile = { ...this.profile!, ...profile };
+      });
+    } catch (error) {
+      toast.error('Problem updating the  profile.');
+    }
+  };
 }
