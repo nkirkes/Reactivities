4ff66e35f09745108ed4cfdfe2064b8471ab33f0
nkirkes <nick.kirkes@gmail.com>
2020-05-11 16:45:01 -0400

    finishing up follower/following ui changes

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -87,7 +87,10 @@ const Profiles = {
   updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles/`, profile),
   uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
   setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-  deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+  deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+  follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
+  unfollow: (username: string) => requests.del(`/profiles/${username}/follow`),
+  listFollowings: (username: string, predicate: string) => requests.get(`/profiles/${username}/follow?predicate=${predicate}`)
 };
 
 export default {
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -13,21 +13,36 @@ interface RouteParams {
 
 interface IProps extends RouteComponentProps<RouteParams> {}
 
-const ProfilePage: React.FC<IProps> = ({match}) => {
+const ProfilePage: React.FC<IProps> = ({ match }) => {
   const rootStore = useContext(RootStoreContext);
-  const {loadingProfile, profile, loadProfile} = rootStore.profileStore;
+  const {
+    loadingProfile,
+    profile,
+    loadProfile,
+    follow,
+    unfollow,
+    isCurrentUser,
+    loading,
+    setActiveTab
+  } = rootStore.profileStore;
 
   useEffect(() => {
-    loadProfile(match.params.username)
-  }, [loadProfile, match])
+    loadProfile(match.params.username);
+  }, [loadProfile, match]);
 
-  if (loadingProfile) return <LoadingComponent content='Loading profile...' />
+  if (loadingProfile) return <LoadingComponent content="Loading profile..." />;
 
   return (
     <Grid>
       <Grid.Column width={16}>
-        <ProfileHeader profile={profile!} />
-        <ProfileContent />
+        <ProfileHeader
+          profile={profile!}
+          isCurrentUser={isCurrentUser}
+          loading={loading}
+          follow={follow}
+          unfollow={unfollow}
+        />
+        <ProfileContent setActiveTab={setActiveTab} />
       </Grid.Column>
     </Grid>
   );
